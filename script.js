d75350aeb7b73da155ee8657d9b728d5bab544eb
ZaheerK-pro <jahirkhanis665@gmail.com>
2024-10-01 06:57:45 +0530

    Adding sound effects

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ const winningCombinations = [
     [6, 4, 2]
 ];
 
+// Add audio elements to play winning and draw sounds
+const winSound = new Audio('res/win-sound.wav');
+const drawSound = new Audio('res/draw-sound.wav');
+
 function setup() {
     showCurrentPlayer();
 
@@ -26,7 +30,7 @@ function setup() {
     document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(0)';
     document.getElementById('line-6').style.transform = 'rotate(45deg) scaleX(0)';
     document.getElementById('line-7').style.transform = 'rotate(135deg) scaleX(0)';
-};
+}
 
 function fillField(field) {
     if(!document.getElementById('field-' + field).classList.contains('disabled-td')) {
@@ -36,14 +40,14 @@ function fillField(field) {
         } else {
             currentShape = 'cross';
             crossFields.push(field);
-        };
+        }
         
         disableField(field);
         drawShapeAt(field);
         checkWin();
         showCurrentPlayer();
-    };
-};
+    }
+}
 
 function showCurrentPlayer() {
     if(currentShape == 'cross') {
@@ -52,41 +56,41 @@ function showCurrentPlayer() {
     } else {
         document.getElementById('x-player-info').style.opacity = 1;
         document.getElementById('o-player-info').style.opacity = 0.5;
-    };
-};
+    }
+}
 
-function drawShapeAt(field){
+function drawShapeAt(field) {
     if(crossFields.includes(field)) {
         document.getElementById('field-' + field).style.backgroundImage = 'url("res/cross.png")';
     } else {
         document.getElementById('field-' + field).style.backgroundImage = 'url("res/circle.png")';
-    };
-};
+    }
+}
 
 function disableField(field) {
     document.getElementById('field-' + field).classList.add('disabled-td');
-};
+}
 
 function checkWin() {
     /**check for winner */
     for(i = 0; i < winningCombinations.length; i++) {
-        if(crossFields.includes(winningCombinations[i][0]) && crossFields.includes(winningCombinations[i][1]) && crossFields.includes(winningCombinations[i][2])){
+        if(crossFields.includes(winningCombinations[i][0]) && crossFields.includes(winningCombinations[i][1]) && crossFields.includes(winningCombinations[i][2])) {
             winner = 'cross';
             winnersFields = winningCombinations[i];
         } else if(circleFields.includes(winningCombinations[i][0]) && circleFields.includes(winningCombinations[i][1]) && circleFields.includes(winningCombinations[i][2])) {
             winner = 'circle';
             winnersFields = winningCombinations[i];
-        };
-    };
+        }
+    }
 
-    if(winner){
+    if(winner) {
         drawLine(winningCombinations.indexOf(winnersFields));
         addScore(winner);
         showWinnerContainer(winner);
-    } else if(crossFields.length + circleFields.length == 9){
+    } else if(crossFields.length + circleFields.length == 9) {
         showWinnerContainer('draw');
-    };
-};
+    }
+}
 
 function drawLine(lineNo) {
     const line = document.getElementById('line-' + lineNo);
@@ -96,8 +100,7 @@ function drawLine(lineNo) {
     setTimeout(() => {
         line.style.transform = line.style.transform.replace('scaleX(0)', 'scaleX(1)');
     },0);
-};
-
+}
 
 function showWinnerContainer(winner) {
     const winnerContainer = document.getElementById('winning-container');
@@ -114,8 +117,24 @@ function showWinnerContainer(winner) {
         winnerContainer.style.opacity = 1;
     }, 750);
 
+
+
     updateWinnerContainerGraphics(winner);
-};
+}
+
+function triggerWinEffect() {
+    document.body.classList.add('confetti');
+    setTimeout(() => {
+        document.body.classList.remove('confetti'); // Remove the effect after 3 seconds
+    }, 1000); // 3 seconds effect duration
+}
+
+function triggerDrawEffect() {
+    document.body.classList.add('draw-flash');
+    setTimeout(() => {
+        document.body.classList.remove('draw-flash'); // Remove the effect after 2 seconds
+    }, 2000); // 2 seconds effect duration
+}
 
 function updateWinnerContainerGraphics(winner) {
     const winnerContainerImage = document.getElementById('winning-container-img');
@@ -131,21 +150,22 @@ function updateWinnerContainerGraphics(winner) {
         winnerTopLabel.innerHTML = 'Player';
         winnerBottomLabel.innerHTML = 'won!';
         document.getElementById('winning-container-img').src = 'res/' + winner + '.png';
-    };
-};
+    }
+}
 
 function addScore(to) {
     document.getElementById(to + '-player-wins').innerHTML = Number(document.getElementById(to + '-player-wins').innerHTML) + 1;
-};
-
+}
 
 function resetAll() {
     document.getElementById('winning-container').classList.add('hide');
-
     showCurrentPlayer();
     resetAllFields();
     resetAllValues();
-};
+
+    // Remove confetti or draw effect
+    document.body.classList.remove('confetti', 'draw-flash');
+}
 
 function resetAllValues() { 
     crossFields = [];
@@ -157,11 +177,11 @@ function resetAllValues() {
 
 function resetAllFields() {
     for(i = 0; i < 9; i++) {
-        if(i != 8){
+        if(i != 8) {
             document.getElementById('line-' + i).style.transitionDuration = '0ms';
             document.getElementById('line-' + i).style.transform = document.getElementById('line-' + i).style.transform.replace('scaleX(1)', 'scaleX(0)');
-        };
+        }
         document.getElementById('field-' + i).style.backgroundImage = '';
         document.getElementById('field-' + i).classList.remove('disabled-td');
-    };
-};
+    }
+}
